d7e9b6dc2da1e50bd4db5dc51c224052a48e5e5f
AndrewMik <Andrew.Mikitchuk@gmail.com>
2018-02-23 18:45:27 +0300

    Task_Money Exchange: Solution of the task 5

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    if (currency <= 0) return {};
+    if (currency >= 10000) return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+
+    var coins = {
+        H: 50,
+        Q: 25,
+        D: 10,
+        N: 5,
+        P: 1
+    }
+
+    var coinsToBeGiven = {};
+
+    for (coin in coins) {
+        if (currency / coins[coin] >= 1) {
+            coinsToBeGiven[coin.toString()] = Math.floor(currency / coins[coin]).toString();
+            currency -= coins[coin] * coinsToBeGiven[coin.toString()];
+        }
+    }
+
+    return coinsToBeGiven;
 }
